0b3143c666961783ebceb9a013f5f3e0c647ab71
priyanshu negi <pnuk1895@gmail.com>
2026-09-05 14:56:30 +0530

    new update

--- src/component/HomeComponents/TopBar.jsx
+++ src/component/HomeComponents/TopBar.jsx
@@ -12,11 +12,18 @@ export default function TopBar() {
     let toggleSpan="block h-0.5 w-full bg-[#1e8e3e] rounded-xs"
 
     return (
-        <header className=" w-screen bg-[#ffffff] border-b border-[#eef1ee] sticky top-0 z-50 font-custom overflow-hidden  " >
+        <header className=" w-full bg-[#ffffff] border-b border-[#eef1ee] sticky top-0 z-50 font-custom overflow-hidden  " >
             <div className=" mx-auto px-10 py-4 flex justify-between items-center gap-6  ">
 
+                {/* <!-- Mobile menu toggle --> */}
+                <button className="hidden max-[1444px]:flex max-[1444px]:-order-1 flex-col justify-center gap-1.5 w-8 h-8  bg-transparent border-none cursor-pointer shrink-0 " aria-label="Open menu">
+                    <span className={toggleSpan}></span>
+                    <span className={toggleSpan}></span>
+                    <span className={toggleSpan}></span>
+                </button>
+
                 {/* <!-- Logo -->Name */}
-                <div className="flex items-center gap-2 shrink-0 max-[1444px]:absolute left-26 ">
+                <div className="flex items-center gap-2 shrink-0  ">
                     <img src={logo} className="w-10 h-10 flex justify-center items-center rounded-2xl " >
                     </img>
                     <span className="flex flex-col ">
@@ -56,21 +63,16 @@ export default function TopBar() {
                         <span>Login / Signup</span>
                     </Link>
 
-                    <Link to="#" className="relative flex items-center gap-2.5 md:bg-[#1e8e3e] md:text-[#ffffff] px-4.5 py-2.5 rounded-lg text-4 font-semibold transition-background duration-200 ease-linear hover:bg-[#17722f] sm:max-md:text-[#1e8e3e] max-sm:text-black ">
+                    <Link to="#" className="relative flex items-center gap-2.5 md:bg-[#1e8e3e] md:text-[#ffffff] px-4.5 py-2.5 rounded-lg text-base font-semibold transition-colors duration-200 ease-linear hover:bg-[#17722f] sm:max-md:text-[#1e8e3e] max-sm:text-black ">
                         <span className="relative flex items-center">
-                            <ShoppingCart className='md:text-[#ffffff] sm:max:md:text-[#1e8e3e] max-sm:text-black '/>
+                            <ShoppingCart className='md:text-[#ffffff] sm:max-md:text-[#1e8e3e] max-sm:text-black '/>
                         </span>
                         <span className='absolute left-8 top-0.5 max-md:bg-[#1e8e3e] max-md:text-[#ffffff] text-[10px] font-bold w-4 h-4 rounded-full flex items-center justify-center sm:hidden  '>0</span>
                         <span className="cart-total max-sm:hidden ">₹1,248</span>
                     </Link>
                 </div>
 
-                {/* <!-- Mobile menu toggle --> */}
-                <button className="hidden max-[1444px]:flex max-[1444px]:-order-1 flex-col justify-center gap-1.5 w-8 h-8  bg-none border-none cursor-pointer shrink-0 " aria-label="Open menu">
-                    <span className={toggleSpan}></span>
-                    <span className={toggleSpan}></span>
-                    <span className={toggleSpan}></span>
-                </button>
+    
 
             </div>
         </header>
